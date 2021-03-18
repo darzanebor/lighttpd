98f0125f986ba3b96e88eb5b8ec3800e688fe9f8
darzanebor <51837169+darzanebor@users.noreply.github.com>
2021-03-18 18:59:44 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-DEFAULT_CONF='/etc/lighttpd/lighttpd.conf'
-CONF="${LIGHTTPD_CONF:-$DEFAULT_CONF}"
+CONF="${LIGHTTPD_CONF:-/etc/lighttpd/lighttpd.conf}"
 chown -R lighttpd:lighttpd /var/run/lighttpd /var/log/lighttpd /var/cache/lighttpd
 while [ ! -f $CONF ]
 do
